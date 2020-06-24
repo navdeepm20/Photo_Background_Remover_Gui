157b1cc3c25dfe307e55c094db37c5a2fb7334af
Navdeep_Mishra <navdeepmishra82734@gmail.com>
2020-06-24 22:05:46 +0530

    All Frames Packed in a right manner

--- BgRemover.py
+++ BgRemover.py
@@ -7,37 +7,52 @@ class RmGui(Tk):
     def __init__(self):
         super().__init__()
         self.geometry("900x600")
-    def picframe1(self):
-        self.picf1 = Frame(root,relief=SUNKEN,width=450,height=570)
-        self.picf1.pack(side=LEFT,anchor="nw")
-        self.picf2 = Frame(root,relief=SUNKEN,width=450,height=570)
-        self.picf2.pack(side=RIGHT,anchor="ne")
-      
+        self.bottomframe = Frame(self,relief=SUNKEN,width=900,height=25)
+        self.bottomframe.pack(fill=X,side=BOTTOM)
+        self.picf1 = Frame(self,relief=SUNKEN,width=450)
 
+    def picframe1(self):
+        self.picf1.pack(side=LEFT,fill=BOTH,anchor="nw")
+        
+        self.picf2 = Frame(root,relief=SUNKEN,width=450)
+        self.picf2.pack(side=RIGHT,anchor="ne",fill=BOTH)
+      
+    
     def piclabel1(self):  
         pass
 
-    def uploadbutton(self):
-        self.supbt=Button(root,text="Upload")
-        self.supbt.pack(side=RIGHT,anchor="sw")
+    def uploadbutton(self): 
+        self.supbt=Button(self.bottomframe,text="Upload")
+        self.supbt.pack(side=LEFT,anchor="se")
+        
     def downloadbutton(self):
-        self.dwbt=Button(root,text="Download")
-        self.dwbt.pack(side=RIGHT,anchor="sw")
+        
 
-    def fstatusbar(self):
+        self.dwbt=Button(self.bottomframe,text="Download")
+        self.dwbt.pack(side=LEFT,anchor="se")
+    
+    def bottomlabel(self):
         self.cstatus = StringVar()
         self.cstatus.set("Ready")
-        self.sbar = Label(self,textvariable=self.cstatus,relief=SUNKEN)
-       
-        
-        self.downloadbutton()
+        self.sbar = Label(self.bottomframe,textvariable=self.cstatus)
+        self.sbar.pack(side=LEFT,fill=X,expand=1,anchor="sw",ipady=2,pady=(2,0))
+
+    def bottombarframe(self):
+        self.bottomlabel()
         self.uploadbutton()
-        self.sbar.pack(side=LEFT,fill=X,expand=1,anchor="sw",ipady=3)
+        self.downloadbutton()
+        
+       
+
         
 
 
 if __name__=='__main__':
     root = RmGui()
-    # root.picframe1()
-    root.fstatusbar()
+    
+    root.bottombarframe()
+    root.picframe1()
+    
+    # root.downloadbutton()
+    # root.uploadbutton()
     root.mainloop()
