f59e994f244b6aab82a40d9b29ec6c0ad99f45ff
Navdeep_Mishra <navdeepmishra82734@gmail.com>
2020-06-27 22:22:23 +0530

    Now you can open image inside the program. Upload and download button is not fixed yet

--- BgRemover.py
+++ BgRemover.py
@@ -1,7 +1,7 @@
 import requests
 from tkinter import *
 from tkinter.ttk import *
-from tkinter.filedialog import askopenfile
+from tkinter.filedialog import askopenfilename
 from PIL import ImageTk , Image
 class RmGui(Tk):
     
@@ -13,9 +13,18 @@ class RmGui(Tk):
         self.picf1 = Frame(self,relief=SUNKEN,width=450)
 
     def pic_loader(self):
-        path= askopenfile(filetypes=[('JPG Files','*.jpg'),('PNG Files','*.png'),('JPEG Files','*.jpeg')])
-        Img = Image.open(path)
-        self.rImage =ImageTk.PhotoImage(Img)
+        self.ipath= askopenfilename(filetypes=[('JPG Files','*.jpg'),('PNG Files','*.png'),('JPEG Files','*.jpeg')]) #,
+        print(self.ipath)
+        Img = Image.open(self.ipath)
+        oldwidth , oldheight = Img.size
+        olddimension = (oldwidth,oldheight)
+        self.oldpicsize = olddimension
+        newwidth=450
+        newheight = newwidth * oldheight//oldwidth
+        resized=(newwidth,newheight)
+        Img = Img.resize(resized)
+        self.rImage = ImageTk.PhotoImage(Img)
+        # self.rImage.image = Img
         self.piclabel1(self.rImage)
        
     def topmenu(self):
@@ -37,7 +46,7 @@ class RmGui(Tk):
         self.picLabel1.pack()
 
     def uploadbutton(self): 
-        self.supbt=Button(self.bottomframe,text="Upload")
+        self.supbt=Button(self.bottomframe,text="Upload",command=self.uploadbuttonfunctionality)
         self.supbt.pack(side=LEFT,anchor="se")
         
     def downloadbutton(self):
@@ -56,7 +65,23 @@ class RmGui(Tk):
         self.bottomlabel()
         self.uploadbutton()
         self.downloadbutton()
-        
+    def uploadbuttonfunctionality(self):
+        import requests
+
+        response = requests.post(
+                'https://api.remove.bg/v1.0/removebg',
+                files={'image_file': open('self.path', 'rb')},
+                data={'size': 'auto'},
+                headers={'X-Api-Key': 'Vt1fjLMNViXdjxvLmzMq3BpD'},
+            )
+        if response.status_code == requests.codes.ok:
+            with open('no-bg.png', 'wb') as out:
+                out.write(response.content)
+        else:
+            print("Error:", response.status_code, response.text)
+    
+    def downloadbuttonfunctionality(self):
+        pass
        
 
         
@@ -71,3 +96,32 @@ if __name__=='__main__':
     
   
     root.mainloop()
+
+
+# from tkinter import Label,Tk
+# from PIL import Image, ImageTk
+# from tkinter.filedialog import askopenfilename
+# root = Tk()
+
+# path=askopenfilename(filetypes=[("Image File",'.jpg')])
+# im = Image.open(path)
+# tkimage = ImageTk.PhotoImage(im)
+# myvar=Label(root,image = tkimage)
+# myvar.image = tkimage
+# myvar.pack()
+
+# root.mainloop()
+
+# import requests
+
+# response = requests.post(
+# 'https://api.remove.bg/v1.0/removebg',
+# files={'image_file': open('D:\\My Photos\\Nkm Pics\\test.jpg', 'rb')},
+# data={'size': 'auto'},
+# headers={'X-Api-Key': 'Vt1fjLMNViXdjxvLmzMq3BpD'},
+# )
+# if response.status_code == requests.codes.ok:
+#     with open('no-bgg.png', 'wb') as out:
+#         out.write(response.content)
+# else:
+#     print("Error:", response.status_code, response.text)
